a59f5add01a3b12ffd51f4ab3de8165420d4fc5a
aji <a.ji@daum.net>
2013-05-16 17:22:47 +0900

    first

---
+++ daum-best-news.js
@@ -0,0 +1,46 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var step =require('step');
+
+step(
+
+	function readUrl(){
+
+		console.log("readUrl")
+
+		request('http://m.media.daum.net/media/hotview/', this);
+
+	},
+	function parseImg(err, data){
+
+		console.log("parseImg")
+		
+		var $ = cheerio.load(data.body);
+		
+		var result = [];
+
+	 	$('.list_num a').each(function(index, el){
+	 
+	 		result.push( $(el).text() )
+	        // console.log(  );
+	    });
+
+		return result;
+	},
+	function makeFile(err, data){
+
+		console.log("makeFile")
+		console.log(data)
+
+		data = data.join("\n");
+
+	    fs.writeFile('news-title.txt', data, this);
+	},
+	function done(err, data){
+
+		console.log('It\'s saved!');
+	}
+
+);
+
